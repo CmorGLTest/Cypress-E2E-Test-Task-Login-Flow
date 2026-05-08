9576cb2fcb429fea27e9f64b8602a7383ddf9c3b
CmorGLTest <clau.morabi@gmail.com>
2026-05-08 16:31:34 +0200

    Completed project structure reorganization: removed old root-level files

--- commands.ts
+++
@@ -1,6 +0,0 @@
-Cypress.Commands.add('login', (email: string, password: string) => {
-  cy.get('input[name="email"]').type(email)
-  cy.get('input[name="password"]').type(password)
-  cy.get('button[type="submit"]').click()
-})
-cy.login('test@test.com', '123456')
